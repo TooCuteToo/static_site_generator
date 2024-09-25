c76a18935722f17f01c824e0994bd6cc4fcb401a
TooCuteToo <bao.2001181020@gmail.com>
2024-09-25 19:15:11 +0700

    feature: recursive function to copy file from source to dest

--- src/main.py
+++ src/main.py
@@ -1,11 +1,44 @@
+import shutil
 from helper import Helper
+import os
 
 
 def main():
-    str = "# This is a heading\n\nThis is a paragraph of text. It has some **bold** and *italic* words inside of it.\n\n* This is the first list item in a list block\n* This is a list item\n* This is another list item\n"
+    clean_up_dir("public")
+    copy_file_to_dest("static", "public")
 
-    children = Helper.markdown_to_html_node(str)
-    print(children)
+
+def clean_up_dir(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+        return
+
+    print(f"cleaning up the dir: {path}")
+    shutil.rmtree(path)
+
+
+def copy_file_to_dest(source_path, dest_path):
+    if os.path.isfile(source_path):
+        print(f"copying source file: {source_path} --- dest: {dest_path}")
+        shutil.copy(source_path, dest_path)
+        return
+
+    list_dir = os.listdir(source_path)
+
+    for dir in list_dir:
+        s = os.path.join(source_path, dir)
+        d = os.path.join(dest_path, dir)
+        print(f"source path: {s} --- dest path: {d}")
+
+        if os.path.isdir(s):
+            if not os.path.exists(d):
+                print(f"creating dest path: {d}")
+                os.mkdir(d)
+
+            copy_file_to_dest(s, d)
+
+        else:
+            copy_file_to_dest(s, d)
 
 
 if __name__ == "__main__":
